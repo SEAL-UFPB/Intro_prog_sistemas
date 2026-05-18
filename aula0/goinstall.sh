5938a5be3f3d6a72076c9824947968cd3787f4d2
Mikael Menezes <144395049+MikaelMenez@users.noreply.github.com>
2026-05-18 09:17:16 -0300

    Replace curl with wget for downloading Go

--- aula0/goinstall.sh
+++ aula0/goinstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 1. Identifica a versão e baixa
-GO_VERSION=$(curl -s https://go.dev/VERSION?m=text | head -n 1)
+GO_VERSION=$(wget -qO- https://go.dev/VERSION?m=text | head -n 1)
 FILE_NAME="${GO_VERSION}.linux-amd64.tar.gz"
 
 echo "Baixando $GO_VERSION..."
-curl -LO "https://go.dev/dl/${FILE_NAME}"
+wget -q --show-progress "https://go.dev/dl/${FILE_NAME}"
 
 # 2. Limpeza e Extração
 sudo rm -rf /usr/local/go
@@ -13,7 +13,6 @@ sudo tar -C /usr/local -xzf "$FILE_NAME"
 rm "$FILE_NAME"
 
 # 3. Identifica o shell real do usuário
-# Se o comando 'zsh' existir e o arquivo .zshrc também, priorizamos ele
 if [ -f "$HOME/.zshrc" ]; then
     CONF_FILE="$HOME/.zshrc"
 else
@@ -27,4 +26,3 @@ if ! grep -q '/usr/local/go/bin' "$CONF_FILE"; then
 else
     echo "PATH já configurado em $CONF_FILE"
 fi
-
